8b731276151b1dbad73ebf5486d9aa315a8ac260
Eickhoff Bernhard (CC-AD/ESW1) <bernhard.eickhoff@de.bosch.com>
2020-09-25 00:32:08 -0700

    typed benchmarks for vector / list
    Signed-off-by: Eickhoff Bernhard (CC-AD/ESW1) <bernhard.eickhoff@de.bosch.com>

--- iceoryx_utils/test/benchmarks/test_cxx_list_benchmark.cpp
+++ iceoryx_utils/test/benchmarks/test_cxx_list_benchmark.cpp
@@ -16,49 +16,148 @@
 #include "../../../build/dependencies/gbenchmark/src/include/benchmark/benchmark.h"
 #include "iceoryx_utils/cxx/list.hpp"
 #include "iceoryx_utils/cxx/vector.hpp"
+#include <list>
+#include <vector>
 
-using namespace iox::cxx;
+#define ListSizeInTest 256U
 
-static void BM_ListRead(benchmark::State& st)
+
+template <typename ContainerType>
+void fillContainer(ContainerType& c, uint64_t fillCnt)
+{
+    for (uint64_t k = 0; k < fillCnt; ++k)
+    {
+        c.emplace_back(k);
+    }
+}
+
+template <typename ContainerType>
+void iterateContainer(ContainerType& c)
+{
+    auto iter = c.begin();
+    for (uint l = 1; l < c.size(); ++l)
+    {
+        benchmark::DoNotOptimize(++iter);
+        benchmark::ClobberMemory();
+    }
+}
+
+template <typename ContainerTypeFrom, typename ContainerTypeTo>
+void readAndCopyPointerOfContainerElem(ContainerTypeFrom& frC, ContainerTypeTo& toC)
+{
+    for (auto& el : frC)
+    {
+        toC.emplace_back(&el);
+        benchmark::DoNotOptimize(toC.back());
+        benchmark::ClobberMemory();
+    }
+}
+
+template <typename ContainerTypeFrom, typename ContainerTypeTo>
+static void BM_typedListRead(benchmark::State& st)
 {
     for (auto _ : st)
     {
         st.PauseTiming();
-        vector<int*, 256> v;
-        list<int, 256> testContainer;
-        for (uint64_t k = 0; k < testContainer.capacity(); ++k)
-        {
-            testContainer.emplace_back(k);
-        }
+        ContainerTypeTo v;
+        ContainerTypeFrom testContainer;
+        fillContainer<ContainerTypeFrom>(testContainer, st.range(0));
         st.ResumeTiming();
 
-        for (auto& el : testContainer)
-        {
-            benchmark::DoNotOptimize(v.emplace_back(&el));
-        }
+        readAndCopyPointerOfContainerElem(testContainer, v);
     }
 }
-BENCHMARK(BM_ListRead);
+BENCHMARK_TEMPLATE(BM_typedListRead, iox::cxx::vector<int, ListSizeInTest>, iox::cxx::vector<int*, ListSizeInTest>)
+    ->Arg(ListSizeInTest);
+BENCHMARK_TEMPLATE(BM_typedListRead, std::list<int>, iox::cxx::vector<int*, ListSizeInTest>)->Arg(ListSizeInTest);
+BENCHMARK_TEMPLATE(BM_typedListRead, iox::cxx::list<int, ListSizeInTest>, iox::cxx::vector<int*, ListSizeInTest>)
+    ->Arg(ListSizeInTest);
 
-static void BM_VectorRead(benchmark::State& st)
+
+template <typename ContainerType>
+static void BM_typedListIncr(benchmark::State& st)
 {
     for (auto _ : st)
     {
         st.PauseTiming();
-        vector<int*, 256> v;
-        vector<int, 256> testContainer;
-        for (uint64_t k = 0; k < testContainer.capacity(); ++k)
+        ContainerType testContainer;
+        fillContainer<ContainerType>(testContainer, st.range(0));
+        st.ResumeTiming();
+
+        iterateContainer(testContainer);
+    }
+}
+BENCHMARK_TEMPLATE(BM_typedListIncr, iox::cxx::vector<int, ListSizeInTest>)->Arg(ListSizeInTest);
+BENCHMARK_TEMPLATE(BM_typedListIncr, std::list<int>)->Arg(ListSizeInTest);
+BENCHMARK_TEMPLATE(BM_typedListIncr, iox::cxx::list<int, ListSizeInTest>)->Arg(ListSizeInTest);
+
+
+template <typename ContainerType>
+static void BM_typedListRemove(benchmark::State& st)
+{
+    for (auto _ : st)
+    {
+        st.PauseTiming();
+        ContainerType testContainer;
+        fillContainer<ContainerType>(testContainer, st.range(0));
+
+        auto iter = testContainer.begin();
+        for (int64_t k; k < st.range(1); ++k)
         {
-            testContainer.emplace_back(k);
+            ++iter;
         }
         st.ResumeTiming();
 
-        for (auto& el : testContainer)
-        {
-            benchmark::DoNotOptimize(v.emplace_back(&el));
-        }
+        benchmark::DoNotOptimize(testContainer.erase(iter));
+        benchmark::ClobberMemory();
     }
 }
-BENCHMARK(BM_VectorRead);
+BENCHMARK_TEMPLATE(BM_typedListRemove, iox::cxx::vector<int, ListSizeInTest>)
+    ->Args({ListSizeInTest, 1U})
+    ->Args({ListSizeInTest, ListSizeInTest / 2U})
+    ->Args({ListSizeInTest, ListSizeInTest - 1U});
+BENCHMARK_TEMPLATE(BM_typedListRemove, std::list<int>)
+    ->Args({ListSizeInTest, 1U})
+    ->Args({ListSizeInTest, ListSizeInTest / 2U})
+    ->Args({ListSizeInTest, ListSizeInTest - 1U});
+BENCHMARK_TEMPLATE(BM_typedListRemove, iox::cxx::list<int, ListSizeInTest>)
+    ->Args({ListSizeInTest, 1U})
+    ->Args({ListSizeInTest, ListSizeInTest / 2U})
+    ->Args({ListSizeInTest, ListSizeInTest - 1U});
+
+template <typename ContainerType>
+static void BM_typedListIterateAndRemove(benchmark::State& st)
+{
+    for (auto _ : st)
+    {
+        st.PauseTiming();
+        ContainerType testContainer;
+        fillContainer<ContainerType>(testContainer, st.range(0));
+        st.ResumeTiming();
+
+        auto iter = testContainer.begin();
+        for (int64_t k; k < st.range(1); ++k)
+        {
+            benchmark::DoNotOptimize(++iter);
+            benchmark::ClobberMemory();
+        }
+
+        benchmark::DoNotOptimize(testContainer.erase(iter));
+        benchmark::ClobberMemory();
+    }
+}
+
+BENCHMARK_TEMPLATE(BM_typedListIterateAndRemove, iox::cxx::vector<int, ListSizeInTest>)
+    ->Args({ListSizeInTest, 1U})
+    ->Args({ListSizeInTest, ListSizeInTest / 2U})
+    ->Args({ListSizeInTest, ListSizeInTest - 1U});
+BENCHMARK_TEMPLATE(BM_typedListIterateAndRemove, std::list<int>)
+    ->Args({ListSizeInTest, 1U})
+    ->Args({ListSizeInTest, ListSizeInTest / 2U})
+    ->Args({ListSizeInTest, ListSizeInTest - 1U});
+BENCHMARK_TEMPLATE(BM_typedListIterateAndRemove, iox::cxx::list<int, ListSizeInTest>)
+    ->Args({ListSizeInTest, 1U})
+    ->Args({ListSizeInTest, ListSizeInTest / 2U})
+    ->Args({ListSizeInTest, ListSizeInTest - 1U});
 
 BENCHMARK_MAIN();
