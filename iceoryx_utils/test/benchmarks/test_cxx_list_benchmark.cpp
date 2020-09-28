6c5db95a5d23bb9bf5d84427510e76fa83ae96e7
Eickhoff Bernhard (CC-AD/ESW1) <bernhard.eickhoff@de.bosch.com>
2020-09-27 23:46:35 -0700

    cleanup includes in benchmark test case
    Signed-off-by: Eickhoff Bernhard (CC-AD/ESW1) <bernhard.eickhoff@de.bosch.com>

--- iceoryx_utils/test/benchmarks/test_cxx_list_benchmark.cpp
+++ iceoryx_utils/test/benchmarks/test_cxx_list_benchmark.cpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//#include "test.hpp"
-#include "../../../build/dependencies/gbenchmark/src/include/benchmark/benchmark.h"
+#include <benchmark/benchmark.h>
+
 #include "iceoryx_utils/cxx/list.hpp"
 #include "iceoryx_utils/cxx/vector.hpp"
 
